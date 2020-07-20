ed59cf33749023715f8281c3e6547da2346e56de
Astario <mokatev@yandex.ru>
2020-07-20 14:25:34 +0300

    test deleted

--- src/js/__tests__/PaymentSystemCheck_test.js
+++
@@ -1,9 +0,0 @@
-// import {paymentSystemCheck} from '../paymentSystemCheck';
-// import { CardWidget } from '../CardWidget';
-//
-// test('Вернет null', () => {
-//   const w = new CardWidget(document.querySelector('#container'));
-//   w.bindToDOM();
-// const result = paymentSystemCheck('ghgf');
-// expect(result).toBe(null);
-// });
